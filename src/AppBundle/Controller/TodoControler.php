cc825085bbebb5f6d64f1fbc53d91810746c5d63
Miyuru <miyuru@CRM4.local>
2016-09-15 10:40:48 +0530

    form edit

--- src/AppBundle/Controller/TodoControler.php
+++ src/AppBundle/Controller/TodoControler.php
@@ -11,17 +11,23 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\TodoDatabase;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use JMS\SerializerBundle\Serializer\Serializer;
 
 class TodoControler extends Controller
 {
     /**
      * @Route("todo/list", name="todo_list")
+     * @Method("GET")
      */
     public function listAction(){
+        /*
         $todos = $this-> getDoctrine()
             -> getRepository('AppBundle:TodoDatabase')
             ->findAll();
@@ -30,12 +36,36 @@ class TodoControler extends Controller
         return $this -> render('todo/index.html.twig', array(
             'todos' => $todos
         ));
+        */
+        $em = $this->getDoctrine()->getManager();
+        $todos = $em->getRepository('AppBundle:TodoDatabase')->findAll();
+        $todos = $this->get('serializer')->serialize($todos,'json');
+        $respose = new Response($todos);
+        $respose->headers->set('Content-Type','application/json');
+        return $respose;
+
     }
+
     /**
-     * @Route("todo/add2", name="todo_add")
+     * @Route("todo/index", name="display")
+     */
+    public function display(){
+        return $this -> render('todo/index.html.twig');
+    }
+
+
+    /**
+     * @Route("todo/add", name="form")
+     */
+    public function form(){
+        return $this -> render('todo/add2.html.twig');
+    }
+
+    /**
+     * @Route("todo/add2")
      */
     public function addAction(Request $request){
-
+    /*
         $todo = new TodoDatabase();
         $form = $this ->createFormBuilder($todo)
             ->add('todowork', TextType::class, array('attr'=> array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
@@ -64,6 +94,26 @@ class TodoControler extends Controller
 
 
         return $this -> render('todo/add2.html.twig');
+    */
+
+            $data = json_decode($request->getContent(), true);
+            $todo = new TodoDatabase();
+
+            $todowork = $data['todoText'];
+            $todo->setTodowork($todowork);
+            $todo->setStatus(0);
+
+            $em = $this-> getDoctrine()->getManager();
+            $em->persist($todo);
+            $em->flush();
+            $this->addFlash(
+                'Notice',
+                'todo Added'
+            );
+        return $this-> redirectToRoute('display');
+
+
+
     }
 
     /**
@@ -89,7 +139,7 @@ class TodoControler extends Controller
         $em-> remove($todo);
         $em->flush();
         $this-> addFlash('noticed','Todo Removed');
-        return $this-> redirectToRoute('todo_list');
+        return $this-> redirectToRoute('display');
     }
 
     /**
--- web/js/todofunction.js
+++ web/js/todofunction.js
@@ -1,8 +1,42 @@
 /**
  * Created by miyuru on 9/13/16.
  */
-var app = angular('todoApp',[]);
-app.controller('todoController',function ($scope) {
-    $scope.msg = "KAVISHAN";
+var app = angular.module('todoApp',[]);
+app.controller('todoController',function ($scope, $http) {
+    $scope.data = {};
+    $scope.addTodo = function(todo){
+        $scope.data = angular.copy(todo);
+
+        var config = {
+            headers : {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+
+        $http.post('http://127.0.0.1:8000/todo/add2', $scope.data, config)
+            .success(function (data, status, headers, config) {
+
+                $scope.PostDataResponse = data;
+
+                //$scope.PostDataResponse = "sucsess";
+            })
+            .error(function (data, status, header, config) {
+                $scope.ResponseDetails = "Data: " + data +
+                    "<hr />status: " + status +
+                    "<hr />headers: " + header +
+                    "<hr />config: " + config;
+            });
+        $scope.data = '';
+
     }
-)
+
+    $http.get("http://127.0.0.1:8000/todo/list")
+        .then(function(response) {
+            $scope.todos = response.data;
+        }, function(response) {
+            $scope.todos = "Error";
+        });
+
+
+
+})
