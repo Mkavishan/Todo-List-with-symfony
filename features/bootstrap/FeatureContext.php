d46906e178843d19c5b0cfe3bc14c5ea69c1577d
Miyuru <miyuru@CRM4.local>
2016-09-27 09:12:33 +0530

    clear

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -10,38 +10,7 @@ require_once 'PHPUnit/Framework/Assert/Functions.php';
 
 class FeatureContext  extends MinkContext implements Context, SnippetAcceptingContext
 {
-    private  $Adder;
-    /**
-     * @Given I have the number :arg1 and the number :arg2
-     */
-    public function iHaveTheNumberAndTheNumber($a, $b)
-    {
 
-        $this->Adder = new \AppBundle\Controller\hello($a,$b);
-        //throw new PendingException();
-    }
-
-    /**
-     * @When I add the together
-     */
-    public function iAddTheTogether2()
-    {
-
-        $this->Adder->add();
-       // throw new PendingException();
-    }
-
-    /**
-     * @Then I should get :arg1
-     */
-    public function iShouldGet($sum)
-    {
-        if($this->Adder->sum != $sum){
-            throw new Exception("Actual sum : ".$this->Adder->sum);
-        }
-        $this->Adder->display();
-        //throw new PendingException();
-    }
     private $test;
     private $addform;
     public function __construct()
